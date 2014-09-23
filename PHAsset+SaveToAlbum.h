858fc985316940e7305bc4bc3992a5cb86aae3c9
Zakk Hoyt <vaporwarewolf@gmail.com>
2014-09-22 19:55:52 -0700

    Update PHAsset+SaveToAlbum.h

--- PHAsset+SaveToAlbum.h
+++ PHAsset+SaveToAlbum.h
@@ -6,11 +6,15 @@
 //  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
+#ifdef __IPHONE_8_0
 @import Photos;
+#endif
 
 typedef void (^PHAssetBoolBlock)(BOOL success);
 
 @interface PHAsset (SaveToAlbum)
 // Create album if it doesn't exist then add PHAsset (self) to it.
--(void)saveToAlbum:(NSString*)album completionBlock:(PHAssetBoolBlock)completionBlock;
+-(void)saveToAlbum:(NSString*)album completionBlock:(PHAssetBoolBlock)completionBlock NS_AVAILABLE_IOS(8_0);
 @end
